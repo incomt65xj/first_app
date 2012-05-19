2ed18b74f523d5e27641b2db6c4de5436280eff1
Incom-T65 XJ <incomt65.xj@gmail.com>
2012-05-19 00:54:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '1fc5cdab3a3399fbd644ef02fc703b966397607be09d3cac3f036acf5f80a15403584e120d1558bf977c6328238cfb8a6cb55dc011920c203cb2d9c0770694d0'
